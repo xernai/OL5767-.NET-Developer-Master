8b53df24d234aa153527664607a9595279592ab0
xabier <montcouronne@hotmail.com>
2026-05-16 13:07:50 -0600

    Initial commit

---
+++ NETBasics/NETBasics/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETBasics1
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        private decimal Costo { get; set; }
+        public void Comer()
+        {
+            Console.WriteLine("Como");
+        }
+
+        public void Correr()
+        {
+            Console.WriteLine("Corro");
+        }
+
+        public void Cantar()
+        {
+            Console.WriteLine("Trino");
+        }
+
+        public void Cantar(string nombre)
+        {
+            Console.WriteLine("Gruño");
+        }
+    }
+}
---
+++ NETBasics/NETBasics/Program.cs
@@ -0,0 +1,75 @@
+﻿using NETBasics1;
+using System;
+
+namespace NETBasics
+{
+    class Vertebrado
+    {
+
+    }
+
+    interface Vuelo
+    {
+        void Volar();
+    }
+
+    class Oso : Animal
+    {
+    }
+
+    class Pajaro : Animal, Vuelo
+    {
+        public void Volar()
+        {
+            Console.WriteLine("Vuelo");
+        }
+    }
+
+    internal class Program
+    {
+        // OOP
+        // Abstraction
+        // Encapsulation
+        // Inheritance
+        // Polimorphism
+
+        // Tipos de datos
+        // 1. Tipos por valor: int
+        //     1.1 Los valores se guardan en la memoria llamada stack
+        // 2. Tipos por referencia: string, class, interface
+        //    2.1 Se guarda la dirección de memoria de la llamada memoria heap en la memoria stack
+        //    2.2 Los datos que debe almacenar la variable se guardan en la memoria heap
+
+        static void Main(string[] args)
+        {
+            int edad = 20;
+            edad = 30;
+
+            string nombre = "Juan";
+
+            Console.WriteLine($"Mi nombre es {nombre} y mi edad es: {edad}");
+
+            // ------- Instancia de la clase animal
+            //Animal animal = new Animal();
+            //animal.Comer();
+
+            //animal.Correr();
+
+            Oso oso = new Oso();
+            oso.Comer();
+            oso.Correr();
+            oso.Cantar("oso");
+
+            // -- pajaro --
+            Pajaro pajaro = new Pajaro();
+            pajaro.Comer();
+            pajaro.Correr();
+            pajaro.Volar();
+            pajaro.Cantar();
+
+            // Como pueden ver las interfaces no se pueden instanciar
+            // Vuelo vuelo = new Vuelo();
+
+        }
+    }
+}
